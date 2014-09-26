8c63ef3bb1b54bef53b7e0dc9d8a02c2a88a4795
stevenseligsohn <stevenseligsohn@gmail.com>
2014-09-25 22:33:42 -0400

    Work on p2

--- 4784project/neuronNoStim.m
+++ 4784project/neuronNoStim.m
@@ -12,16 +12,18 @@ El = 10.6;
 Vrest = -70;
 
 subplot(2, 1, 1);
-plot(0:.01:1, Vrest, '-.');
+plot(0:0.001:1, Vrest, 'k');
 title('Resting Membrane Potential');
 xlabel('time (s)');
 ylabel('V (V)');
 
 subplot(2, 1, 2);
-plot(0:.01:1, (gna/gk), '--');
+hp = plot(0:.001:1, gna, 'r', 0:.001:1, gk, 'b');
 title('Sodium vs Potassium Channel Conductance');
 xlabel('time (s)');
-
+ylabel('conductance (S/cm^2)');
+hold on;
+legend(hp, 'g_N_a', 'g_K');
 
 end
 
