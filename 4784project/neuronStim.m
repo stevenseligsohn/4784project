ffb5aa90b3aeee269e11ee264bd57d21cb642aa5
stevenseligsohn <stevenseligsohn@gmail.com>
2014-09-27 17:11:11 -0400

    Finalizing project file names and file input

--- 4784project/neuronStim.m
+++ 4784project/neuronStim.m
@@ -1,6 +1,16 @@
-function [  ] = neuronStepStim( )
+function [  ] = neuronStepStim( mode )
 %UNTITLED2 Summary of this function goes here
 %   Detailed explanation goes here
+%input: mode 0 = no current input to system
+%   mode 1 = pulse current input
+%   mode 2 = unit step current input
+if mode>2
+    fprintf('Invalid input.\n')
+    return
+elseif mode<0
+    fprintf('Invalid input.\n')
+    return
+end
 
 %constants
 gk = 36; %mS/cm2
@@ -10,7 +20,7 @@ Ek = -12; %mV
 Ena = 115; %mV
 El = 10.6; %mV
 Vrest = -70; %mV
-deltax=0.001; 
+deltax=.001; 
 
 %general loop vars
 Vm = Vrest;
@@ -46,20 +56,24 @@ conna = []; %gk array
 
 for it=deltax:deltax:100 %ms
     if it<.5 %apply 5uA/cm^2 for 0.5ms
-        %I=500; %V/s
+        if mode~=0 
+            I=5E-2; %V/s
+        end
     else 
-        I=0;
+        if mode==1
+            I=0;
+        end
     end
     %potassium
     alphan = 0.01*((10-Vm)/(exp((10-Vm)/10)-1));
     betan = 0.125*exp(-Vm/80);
     dndt = (alphan*(1-n))-(betan*n);
     dVkdt = Ik/Cm;
-    Ik = (n^4)*gk*(Vm-Ek);
+    Ik = (n^4)*gk*(Vm-Ek); 
     n = n + deltax*dndt; %Euler's
     %Ek = Ek + deltax*dVkdt;
     %poty(round(it/deltax)) = Ek;
-    conk(round(it/deltax)) = (n^4)*gk;
+    conk(round(it/deltax)) = (n^4)*gk; %potassium conductance
 
     %sodium
     alpham = 0.1*((25-Vm)/(exp((25-Vm)/10)-1));
@@ -69,19 +83,22 @@ for it=deltax:deltax:100 %ms
     dmdt = alpham*(1-m)-betam*m;
     dhdt = alphah*(1-h)-betah*h;
     dVnadt = Ina/Cm;
-    Ina = (m^3)*gna*h*(Vm-Ena);
+    Ina = (m^3)*gna*h*(Vm-Ena); 
     m = m + deltax*dmdt; %Euler's
     h = h + deltax*dhdt; 
     %Ena = Ena + deltax*dVnadt;
     %sody(round(it/deltax)) = Ena;
-    conna(round(it/deltax)) = (m^3)*gna*h;
+    conna(round(it/deltax)) = (m^3)*gna*h; %sodium conductance
     
     %general
-    Il = gl*(Vm-El);
+    Il = gl*(Vm-El); 
     Iion = I-Ik-Ina-Il;
     Vm = Vm + deltax*dVmdt;
     dVmdt = Iion/Cm;
     vmx(round(it/deltax)) = it;
+    kder(round(it/deltax)) = dVkdt;
+    nader(round(it/deltax)) = dVnadt;
+    vmder(round(it/deltax)) = dVmdt;
     Icurr(round(it/deltax)) = Iion;
     potcurr(round(it/deltax)) = Ik;
     sodcurr(round(it/deltax)) = Ina;
@@ -89,25 +106,27 @@ for it=deltax:deltax:100 %ms
     vmy(round(it/deltax)) = Vm;
 end
 
-subplot(3, 1, 1);
+subplot(2, 1, 1);
+%plot(vmx, vmy, 'k', vmx, kder, vmx, nader, vmx, vmder);
 plot(vmx, vmy, 'k');
 title('Membrane Potential');
 xlabel('time (ms)');
-ylabel('V (V)');
+ylabel('V (mV)');
+%legend('Vm', 'dVkdt', 'dVnadt', 'dVmdt');
 
-subplot(3, 1, 2);
+subplot(2, 1, 2);
 plot(vmx, conk, 'r', vmx, conna, 'b');
 title('Channel Conductances');
 xlabel('time (ms)');
 ylabel('conductance (S/cm^2)');
 legend('g_K', 'g_N_a');
 
-subplot(3, 1, 3);
-plot(vmx, Icurr, vmx, potcurr, vmx, sodcurr, vmx, calcurr);
-title('Ion Current');
-xlabel('time (ms)');
-ylabel('I (A)');
-legend('Icurr', 'potcurr', 'sodcurr', 'calcurr');
+%subplot(3, 1, 3);
+%plot(vmx, Icurr, vmx, potcurr, vmx, sodcurr, vmx, calcurr);
+%title('Ion Current');
+%xlabel('time (ms)');
+%ylabel('I (mA)');
+%legend('Iion', 'Ik', 'Ina', 'Il');
 
 end
 
